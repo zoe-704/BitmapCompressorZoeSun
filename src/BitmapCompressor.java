99e2579ff3dcbc86695fcef45b1a9365d095cbc9
zoe-704 <sunzoey@gmail.com>
2025-11-29 21:07:12 -0800

    Optimized

--- src/BitmapCompressor.java
+++ src/BitmapCompressor.java
@@ -16,8 +16,6 @@
  *  1240 bits
  ******************************************************************************/
 
-import java.util.function.ObjIntConsumer;
-
 /**
  *  The {@code BitmapCompressor} class provides static methods for compressing
  *  and expanding a binary bitmap input.
@@ -28,7 +26,14 @@ import java.util.function.ObjIntConsumer;
  *  @author Zoe Sun
  */
 public class BitmapCompressor {
+    // bit sizes of compressed sequences for various values of BYTES
+    // 4: 816. 3048
+    // 5: 792, 2008 (2800)
+    // 6: 920, 1832 (2752)
 
+    // Length of encoded sequences
+    public static int BYTES = 5;
+    public static int BITS = (int) Math.pow(2,BYTES)-1;
     /**
      * Reads a sequence of bits from standard input, compresses them,
      * and writes the results to standard output.
@@ -36,24 +41,37 @@ public class BitmapCompressor {
     public static void compress() {
         boolean cur = false; // Assume sequence starts with 0
         int count = 0;
+
+        // Temporary storage of length of 0/1 sequences
+        java.util.ArrayList<Integer> seq = new java.util.ArrayList<>();
+
         // Iterate over entire sequence of bits to compress
         while (!BinaryStdIn.isEmpty()) {
             boolean bit = BinaryStdIn.readBoolean();
             if (bit == cur) { // Part of current sequence
                 count++;
                 // Only using 8 bits to write out run length encoding
-                if (count == 255) {
-                    BinaryStdOut.write(count, 8);
+                if (count == BITS) {
+                    seq.add(count);
                     count = 0;
-                    cur = !cur; // Toggle between 0 and 1
+                    cur = !cur;
                 }
             } else { // Begin new sequence
-                BinaryStdOut.write(count,8);
+                seq.add(count);
                 count = 1; // Current bit is first one of new sequence
                 cur = !cur;
             }
         }
-        BinaryStdOut.write(count, 8);
+        seq.add(count); // Add final sequence
+        int encodedBits = seq.size() * BYTES;
+
+        // 32 bit int header of encoded bits
+        BinaryStdOut.write(encodedBits, 32);
+
+        // Write out all sequence lengths
+        for (int s : seq) {
+            BinaryStdOut.write(s, BYTES);
+        }
         BinaryStdOut.close();
     }
 
@@ -63,10 +81,14 @@ public class BitmapCompressor {
      */
     public static void expand() {
         boolean cur = false; // Assume sequence starts with 0
+
+        // Read in header of the number of encoded bits
+        int encodedBits = BinaryStdIn.readInt(32);
+
         // Iterate over entire sequence of bits to expand
-        while (!BinaryStdIn.isEmpty()) {
-            // 8 bit run length encoding to write out n bits of cur
-            int cur_length = BinaryStdIn.readByte();
+        for (int f = 0; f < encodedBits; f+= BYTES) {
+            // Get length of 0/1 sequence
+            int cur_length = BinaryStdIn.readInt(BYTES);
             for (int i = 0; i < cur_length; i++) {
                 BinaryStdOut.write(cur); // 0 or 1
             }
